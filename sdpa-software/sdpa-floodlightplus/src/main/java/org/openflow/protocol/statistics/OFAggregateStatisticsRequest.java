97070343e182a1942c0908ad151aa5f5be10fff9
zhengzl15 <zhengzl0715@163.com>
2015-09-25 15:30:03 +0800

    new version

--- sdpa-software/sdpa-floodlightplus/src/main/java/org/openflow/protocol/statistics/OFAggregateStatisticsRequest.java
+++
@@ -1,9 +0,0 @@
-package org.openflow.protocol.statistics;
-
-/**
- * Represents an ofp_aggregate_stats_request structure
- * @author Srini Seetharaman (srini.seetharaman@gmail.com)
- */
-public class OFAggregateStatisticsRequest extends OFFlowStatisticsRequest implements OFStatistics {
-
-}
--- sdpa-software/sdpa-ovs2.1.2/include/automake.mk
+++
@@ -1,4 +0,0 @@
-include include/linux/automake.mk
-include include/openflow/automake.mk
-include include/openvswitch/automake.mk
-include include/sparse/automake.mk
